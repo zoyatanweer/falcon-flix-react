7393cf13b94e3033c98dd90302d833aa51e034e1
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2022-06-20 19:41:39 +0530

    FEAT: Added starting files.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import { makeServer } from "./server";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+// makeServer();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// makeServer();
+
 root.render(
+  // makeServer();
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
