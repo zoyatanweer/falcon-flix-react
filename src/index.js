9f8ba240d9f12fd1d3b1d3af5a4699cc00a969d8
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2022-07-14 13:06:54 +0530

    FIX: removed auth

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import { makeServer } from "./server";
+import { makeServer } from "./server";
 
 import reportWebVitals from "./reportWebVitals";
-import { AuthProvider } from "./context/authContext";
+// import { AuthProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// makeServer();
+makeServer();
 
 root.render(
   // makeServer();
 
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      {/* <AuthProvider> */}
+      <App />
+      {/* </AuthProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
